1747f432d0c137037baca2d5796cc3619a132799
Richard Jandušík <xjandus@fi.muni.cz>
2021-09-08 15:18:13 +0200

    fix: missing semi-colons

--- react-test/src/components/Header/index.jsx
+++ react-test/src/components/Header/index.jsx
@@ -5,14 +5,14 @@ import { useSetRecoilState } from 'recoil';
 import menuOnAtom from '../../recoil/menuOn/menuOnAtom';
 
 //Styles
-import { Content } from './Header.styles'
+import { Content } from './Header.styles';
 
 const Header = () => {
   const setMenuOn = useSetRecoilState(menuOnAtom);
 
   const toggleMenuOn = () => {
     setMenuOn(prev => !prev);
-  }
+  };
 
   return (
     <Content>
--- react-test/src/components/Menu/index.jsx
+++ react-test/src/components/Menu/index.jsx
@@ -5,10 +5,10 @@ import { useRecoilValue } from 'recoil';
 import menuOnAtom from '../../recoil/menuOn/menuOnAtom';
 
 //Styles
-import { Content } from './Menu.styles'
+import { Content } from './Menu.styles';
 
 const Menu = () => {
-  const menuOn = useRecoilValue(menuOnAtom)
+  const menuOn = useRecoilValue(menuOnAtom);
 
   return (
     <Content menuOn={menuOn} >
